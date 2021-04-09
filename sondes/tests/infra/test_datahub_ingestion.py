b7046ea3f3eb9953e8d312b2560d840d25794a54
Amine Saboni <asaboni@octo.com>
2021-04-09 17:39:04 +0200

    fix: change hard-coded pathname to dynamic one. added verbose on tests

--- sondes/tests/infra/test_datahub_ingestion.py
+++ sondes/tests/infra/test_datahub_ingestion.py
@@ -43,8 +43,7 @@ def test_create_pipeline_creates_pipeline_given_correct_configuration(dataset_mc
         },
     }
 
-    expected_datahub_source_config = SourceConfig(type='file', config={
-        'filename': '/Users/amine.saboni/Documents/Total/tdf_innovation/sondes/tests/infra/dataset_mce_fixture.json'})
+    expected_datahub_source_config = SourceConfig(type='file', config={'filename': dataset_mce_pathname})
     expected_datahub_sink_config = DynamicTypedConfig(type='datahub-rest', config={'server': 'http://localhost:8080'})
 
     # When
