d24c4fc92bd3982b8d802c127cf8331f6f363219
Ahmed <aalaoui@octo.com>
2021-04-26 17:46:43 +0200

    refacto: deal with expired access token and add tests

--- sondes/src/infra/repository/datagalaxy_dataset_repository.py
+++ sondes/src/infra/repository/datagalaxy_dataset_repository.py
@@ -10,38 +10,44 @@ class DataGalaxyException(Exception):
 
 class DatasetAbstractRepository(abc.ABC):
     @abc.abstractmethod
-    def get_by_id(self, access_token, dataset_id, version_id):
+    def get_by_id(self, dataset_id, version_id):
         return NotImplementedError
 
     @abc.abstractmethod
-    def save_dataset(self, access_token, version_id, dataset_object):
+    def save_dataset(self, version_id, dataset_object):
         return NotImplementedError
 
     @abc.abstractmethod
-    def get_all(self, access_token):
+    def get_all(self):
         return NotImplementedError
 
 
 class DataGalaxyDatasetRepository(DatasetAbstractRepository):
     def __init__(self):
-        integration_token = os.environ.get("DATAGALAXY_INTEGRATION_TOKEN", '')
+        self.integration_token = os.environ.get("DATAGALAXY_INTEGRATION_TOKEN", '')
+        self.access_token = _get_access_token(self.integration_token)
 
-    def get_by_id(self, access_token, dataset_id, version_id):
+    def get_by_id(self, dataset_id, version_id):
         api_url = f'https://api.datagalaxy.com/v2/sources/{version_id}/{dataset_id}'
-        headers = {'Authorization': 'Bearer ' + access_token}
+        headers = {'Authorization': 'Bearer ' + self.access_token}
 
         try:
             api_response = requests.get(api_url, headers=headers)
         except Exception:
             raise DataGalaxyException(f'Error connecting Datagalaxy API ')
         if api_response.status_code != 200:
-            raise DataGalaxyException(f'Error getting API Datagalaxy DATA ')
+            if api_response.status_code == 498:
+                new_access_token = _get_access_token(self.integration_token)
+                headers = {'Authorization': 'Bearer ' + new_access_token}
+                api_response = requests.get(api_url, headers=headers)
+            else:
+                raise DataGalaxyException(f'Error getting API Datagalaxy DATA ')
 
         return api_response.json()
 
-    def save_dataset(self, access_token, version_id, dataset_object):
+    def save_dataset(self, version_id, dataset_object):
         api_url = f'https://api.datagalaxy.com/v2/sources/{version_id}'
-        headers = {'Authorization': 'Bearer ' + access_token}
+        headers = {'Authorization': 'Bearer ' + self.access_token}
         data = {
             "name": dataset_object['name'],
             "status": dataset_object['status'],
@@ -60,34 +66,45 @@ class DataGalaxyDatasetRepository(DatasetAbstractRepository):
         except Exception:
             raise DataGalaxyException(f'Error connecting Datagalaxy API ')
         if api_response.status_code != 200:
-            raise DataGalaxyException(f'Error posting data in Datagalaxy API ')
+            if api_response.status_code == 498:
+                new_access_token = _get_access_token(self.integration_token)
+                headers = {'Authorization': 'Bearer ' + new_access_token}
+                api_response = requests.post(api_url,data=data, headers=headers)
+            else:
+                raise DataGalaxyException(f'Error posting data in Datagalaxy API ')
 
         return api_response.json()
 
-    def get_all(self, access_token):
+    def get_all(self):
         api_url = 'https://api.datagalaxy.com/v2/sources'
-        headers = {'Authorization': 'Bearer ' + access_token}
+        headers = {'Authorization': 'Bearer ' + self.access_token}
 
         try:
             api_response = requests.get(api_url, headers=headers)
         except Exception:
             raise DataGalaxyException(f'Error connecting Datagalaxy API ')
         if api_response.status_code != 200:
-            raise DataGalaxyException(f'Error getting API Datagalaxy DATA ')
+            if api_response.status_code == 498:
+                new_access_token = _get_access_token(self.integration_token)
+                headers = {'Authorization': 'Bearer ' + new_access_token}
+                api_response = requests.get(api_url, headers=headers)
+            else:
+                raise DataGalaxyException(f'Error getting API Datagalaxy DATA ')
 
         return api_response.json()
 
-    def _get_access_token(self, integration_token):
-        api_url = 'https://api.datagalaxy.com/v2/credentials'
-        headers = {'Authorization': 'Bearer ' + integration_token}
 
-        try:
-            api_response = requests.get(api_url, headers=headers)
-        except Exception:
-            raise DataGalaxyException(f'Error connecting Datagalaxy API ')
-        if api_response.status_code != 200:
-            raise DataGalaxyException(f'Error getting API Datagalaxy token ')
+def _get_access_token(integration_token):
+    api_url = 'https://api.datagalaxy.com/v2/credentials'
+    headers = {'Authorization': 'Bearer ' + integration_token}
 
-        api_response_dict = api_response.json()
+    try:
+        api_response = requests.get(api_url, headers=headers)
+    except Exception:
+        raise DataGalaxyException(f'Error connecting Datagalaxy API ')
+    if api_response.status_code != 200:
+        raise DataGalaxyException(f'Error getting API Datagalaxy token ')
 
-        return api_response_dict['accessToken']
+    api_response_dict = api_response.json()
+
+    return api_response_dict['accessToken']
--- sondes/tests/infra/test_datagalaxy_dataset_repository.py
+++ sondes/tests/infra/test_datagalaxy_dataset_repository.py
@@ -1,15 +1,18 @@
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, call
 
 import pytest
-from infra.repository.datagalaxy_dataset_repository import DataGalaxyDatasetRepository, DataGalaxyException
+from src.infra.repository.datagalaxy_dataset_repository import DataGalaxyDatasetRepository, DataGalaxyException, \
+    _get_access_token
 
 
 class TestGetById:
     @patch('src.infra.repository.datagalaxy_dataset_repository.requests.get')
-    def test_get_by_id_returns_request_response_from_api(self, request_get):
+    @patch('src.infra.repository.datagalaxy_dataset_repository._get_access_token',
+           return_value='access_token')
+    def test_get_by_id_returns_request_response_from_api(self, mocked_access_token, request_get):
         # Given
         repository = DataGalaxyDatasetRepository()
-        access_token = 'fake_token'
+
         dataset_id = 1
         version_id = 1
         expected_api_response = {
@@ -25,21 +28,42 @@ class TestGetById:
         response_return_value = MagicMock(status_code=200, text='')
         response_return_value.json = MagicMock(return_value=expected_api_response)
         request_get.return_value = response_return_value
+        mocked_access_token._mock_return_value = 'access_token'
 
         # When
-        actual_api_response = repository.get_by_id(access_token, dataset_id, version_id)
+        actual_api_response = repository.get_by_id(dataset_id, version_id)
 
         # Then
         request_get.assert_called_once_with('https://api.datagalaxy.com/v2/sources/1/1',
-                                            headers={'Authorization': 'Bearer fake_token'})
+                                            headers={'Authorization': 'Bearer access_token'})
 
         assert actual_api_response == expected_api_response
 
     @patch('src.infra.repository.datagalaxy_dataset_repository.requests.get', side_effect=Exception)
-    def test_get_by_id_raises_exception_when_api_call_fails_with_connection_error(self, request_get):
+    @patch('src.infra.repository.datagalaxy_dataset_repository._get_access_token')
+    def test_get_by_id_raises_exception_when_api_call_fails_with_connection_error(self, mocked_access_token,
+                                                                                  request_get):
         # Given
         repository = DataGalaxyDatasetRepository()
-        access_token = 'fake_token'
+
+        dataset_id = 1
+        version_id = 1
+
+        # When
+        with pytest.raises(DataGalaxyException) as datagalaxy_exception:
+            repository.get_by_id(dataset_id, version_id)
+
+        # Then
+        assert str(datagalaxy_exception.value) == "Error connecting Datagalaxy API "
+
+    @patch('src.infra.repository.datagalaxy_dataset_repository.requests.get')
+    @patch('src.infra.repository.datagalaxy_dataset_repository._get_access_token')
+    def test_get_by_id_returns_request_response_from_api_when_using_expired_token_after_recalling_api_with_correct_token(
+            self, mocked_access_token,
+            mocked_request_get):
+        # Given
+        repository = DataGalaxyDatasetRepository()
+
         dataset_id = 1
         version_id = 1
         expected_api_response = {
@@ -52,24 +76,30 @@ class TestGetById:
             "attributes": {}
         }
 
-        response_return_value = MagicMock(status_code=200, text='')
+        response_return_value = MagicMock(status_code=498, text='')
         response_return_value.json = MagicMock(return_value=expected_api_response)
-        request_get.return_value = response_return_value
+        mocked_request_get.return_value = response_return_value
+
+        mocked_access_token.side_effect = ['new_access_token', 'expired_token']
 
         # When
-        with pytest.raises(DataGalaxyException) as datagalaxy_exception:
-            repository.get_by_id(access_token, dataset_id, version_id)
+        actual_api_response = repository.get_by_id(dataset_id, version_id)
 
         # Then
-        assert str(datagalaxy_exception.value) == "Error connecting Datagalaxy API "
+        assert mocked_request_get.call_count == 2
+        assert mocked_request_get.call_args_list[1] == call('https://api.datagalaxy.com/v2/sources/1/1',
+                                                            headers={'Authorization': 'Bearer new_access_token'})
+        assert actual_api_response == expected_api_response
 
 
 class TestSaveDataset:
     @patch('src.infra.repository.datagalaxy_dataset_repository.requests.post')
-    def test_save_dataset_posts_source_in_datagalaxy_api(self, request_post):
+    @patch('src.infra.repository.datagalaxy_dataset_repository._get_access_token',
+           return_value='access_token')
+    def test_save_dataset_posts_source_in_datagalaxy_api(self, mocked_access_token, request_post):
         # Given
         repository = DataGalaxyDatasetRepository()
-        access_token = 'fake_token'
+
         source_object = {
             "name": 'test',
             "status": 'Proposed',
@@ -100,20 +130,22 @@ class TestSaveDataset:
         request_post.return_value = response_return_value
 
         # When
-        actual_api_response = repository.save_dataset(access_token, version_id, source_object)
+        actual_api_response = repository.save_dataset(version_id, source_object)
 
         # Then
         request_post.assert_called_once_with(f'https://api.datagalaxy.com/v2/sources/{version_id}',
-                                             headers={'Authorization': 'Bearer fake_token'},
+                                             headers={'Authorization': 'Bearer access_token'},
                                              data=source_object)
 
         assert actual_api_response == expected_api_response
 
     @patch('src.infra.repository.datagalaxy_dataset_repository.requests.post', side_effect=Exception)
-    def test_save_dataset_raises_exception_when_api_call_fails_with_connection_error(self, request_post):
+    @patch('src.infra.repository.datagalaxy_dataset_repository._get_access_token')
+    def test_save_dataset_raises_exception_when_api_call_fails_with_connection_error(self, mocked_access_token,
+                                                                                     request_post):
         # Given
         repository = DataGalaxyDatasetRepository()
-        access_token = 'fake_token'
+
         source_object = {
             "name": 'test',
             "status": 'Proposed',
@@ -136,18 +168,69 @@ class TestSaveDataset:
 
         # When
         with pytest.raises(DataGalaxyException) as datagalaxy_exception:
-            repository.save_dataset(access_token, version_id, source_object)
+            repository.save_dataset(version_id, source_object)
 
         # Then
         assert str(datagalaxy_exception.value) == "Error connecting Datagalaxy API "
 
+    @patch('src.infra.repository.datagalaxy_dataset_repository.requests.post')
+    @patch('src.infra.repository.datagalaxy_dataset_repository._get_access_token')
+    def test_save_dataset_posts_source_in_datagalaxy_when_using_expired_token_after_recalling_api_with_correct_token(
+            self, mocked_access_token,
+            mocked_request_post):
+        # Given
+        repository = DataGalaxyDatasetRepository()
+
+        source_object = {
+            "name": 'test',
+            "status": 'Proposed',
+            "owners": [
+                'DUC'
+            ],
+            "stewards": [
+                'b2o'
+            ],
+            "tags": [
+                'élie'
+            ],
+            "description": 'test ingestion',
+            "summary": 'Ingestion in datagalaxy',
+            "upsert": True,
+            "type": 'dataset',
+            "technicalName": 'test dataset'
+        }
+        version_id = 1
+
+        expected_api_response = {
+            "id": f'{version_id}',
+            "location": "folder/test"
+        }
+
+        response_return_value = MagicMock(status_code=498, text='')
+        response_return_value.json = MagicMock(return_value=expected_api_response)
+        mocked_request_post.return_value = response_return_value
+
+        mocked_access_token.side_effect = ['new_access_token', 'expired_token']
+
+        # When
+        actual_api_response = repository.save_dataset(version_id, source_object)
+
+        # Then
+        assert mocked_request_post.call_count == 2
+        assert mocked_request_post.call_args_list[1] == call('https://api.datagalaxy.com/v2/sources/1',
+                                                             data=source_object,
+                                                             headers={'Authorization': 'Bearer new_access_token'})
+        assert actual_api_response == expected_api_response
+
 
 class TestGetAll:
     @patch('src.infra.repository.datagalaxy_dataset_repository.requests.get')
-    def test_get_all_sources_returns_request_response_from_api(self, request_get):
+    @patch('src.infra.repository.datagalaxy_dataset_repository._get_access_token',
+           return_value='access_token')
+    def test_get_all_sources_returns_request_response_from_api(self, mocked_access_token, request_get):
         # Given
         repository = DataGalaxyDatasetRepository()
-        access_token = 'fake_token'
+
         expected_api_response = {
             "pages": 0,
             "total": 0,
@@ -184,11 +267,11 @@ class TestGetAll:
         request_get.return_value = response_return_value
 
         # When
-        actual_api_response = repository.get_all(access_token)
+        actual_api_response = repository.get_all()
 
         # Then
         request_get.assert_called_once_with('https://api.datagalaxy.com/v2/sources',
-                                            headers={'Authorization': 'Bearer fake_token'})
+                                            headers={'Authorization': 'Bearer access_token'})
 
         assert actual_api_response == expected_api_response
 
@@ -197,10 +280,9 @@ class TestGetAccessToken:
     @patch('src.infra.repository.datagalaxy_dataset_repository.requests.get')
     def test_get_access_token_returns_correct_access_token_from_integration_token(self, request_get):
         # Given
-        repository = DataGalaxyDatasetRepository()
-        integration_token = 'fake_integration_token'
+        integration_token = 'integration_token'
         expected_api_response = {
-            'accessToken': 'fake_access_token'
+            'accessToken': 'access_token'
         }
         expected_access_token = expected_api_response['accessToken']
 
@@ -209,27 +291,26 @@ class TestGetAccessToken:
         request_get.return_value = response_return_value
 
         # When
-        actual_access_token = repository._get_access_token(integration_token)
+        actual_access_token = _get_access_token(integration_token)
 
         # Then
         request_get.assert_called_once_with('https://api.datagalaxy.com/v2/credentials',
-                                            headers={'Authorization': 'Bearer fake_integration_token'})
+                                            headers={'Authorization': 'Bearer integration_token'})
 
         assert actual_access_token == expected_access_token
 
     @patch('src.infra.repository.datagalaxy_dataset_repository.requests.get')
-    def test_get_access_token_raises_exception_when_api_doesnt_return_access_token(self, request_get):
+    def test_get_access_token_raises_exception_when_using_false_token(self, request_get):
         # Given
-        repository = DataGalaxyDatasetRepository()
-        integration_token = 'fake_integration_token'
+        integration_token = 'false_integration_token'
 
-        response_return_value = MagicMock(status_code=403, text='')
+        response_return_value = MagicMock(status_code=498, text='')
         request_get.return_value = response_return_value
 
         # When
 
         with pytest.raises(DataGalaxyException) as datagalaxy_exception:
-            repository._get_access_token(integration_token)
+            _get_access_token(integration_token)
 
         # Then
         assert str(datagalaxy_exception.value) == "Error getting API Datagalaxy token "
